3ddee6266033136982613b9f2b596a12162cf02c
daniel10027 <danielguedegbe10027@gmail.com>
2020-05-14 18:21:56 +0000

    @Password reset ok

--- learning/urls.py
+++ learning/urls.py
@@ -26,6 +26,18 @@ urlpatterns = [
     path('accounts/signup/tutor/',     tutors.TutorSignUpView.as_view(), name='tutor_signup'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name = 'registration/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'enseignement/index.html'), name="logout"),
+    path('password-reset/',
+                         auth_views.PasswordResetView.as_view(template_name = 'gestion/password_reset.html'),
+                         name="password_reset"),
+    path('password-reset/done/',
+                         auth_views.PasswordResetDoneView.as_view(template_name = 'gestion/password_reset_done.html'),
+                         name="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>/',
+                         auth_views.PasswordResetConfirmView.as_view(template_name = 'gestion/password_reset_confirm.html'),
+                         name="password_reset_confirm"),
+    path('password-reset-complete/',
+                         auth_views.PasswordResetCompleteView.as_view(template_name = 'gestion/password_reset_complete.html'),
+                         name="password_reset_complete"),
 ]
 
 
