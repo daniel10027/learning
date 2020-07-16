9c1eead47039ec2ec017d7445131e2bee81911f1
daniel10027 <danielguedegbe10027@gmail.com>
2020-07-16 10:05:05 +0000

    last

--- learning/settings.py
+++ learning/settings.py
@@ -167,7 +167,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfilesg')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
