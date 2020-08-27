7977707118fc939930b908c1292627db92689800
Daniel Guedegbe <danielguedegbe10027@gmail.com>
2020-08-27 21:57:31 +0000

    Last update
    Smtp

--- learning/settings.py
+++ learning/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w%cuhsdmzmn1zux3xek@%aqx4_s9^zi4)x-@95j%8!mz3f4!+c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -203,8 +202,8 @@ BOOTSTRAP4 = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'eduvroom@gmail.com'
-EMAIL_HOST_PASSWORD = 'ccmutpghovhmqiaa'
+EMAIL_HOST_USER = 'youremail'
+EMAIL_HOST_PASSWORD = 'yourpass'
 EMAIL_PORT = 587
 ############### DEBUG TOOLBAR ####################
 INTERNAL_IPS = [   
